67c91d36b6ead81dcff50011c50dbe457dce90d7
Dulce Abigail Mejía <39814572+dulcemejiav@users.noreply.github.com>
2018-09-20 19:06:43 -0600

    Add files via upload

---
+++ js/my-app.js
@@ -0,0 +1,175 @@
+// Initialize your app
+var myApp = new Framework7({
+	root:'#app',
+	theme: 'md', //ios, md, auto
+    modalTitle: 'Framework7', 	//title for modals
+	panel: {
+    	swipe: 'left',
+  	},
+	cache:false,
+	pushState:false,
+	swipeBackPage:true,
+	material: true,
+	routes: [
+		{
+		path: '/index/',
+    	url: 'index.html',
+    	name: 'index',
+  		},
+		{
+		path: '/envivo/',
+    	url: 'envivo.html',
+    	name: 'envivo',
+  		},
+		{
+		path: '/noticias/',
+    	url: 'noticias.html',
+    	name: 'noticias',
+  		},
+		{
+		path: '/tvcplay/',
+    	url: 'tvcplay.html',
+    	name: 'tvcplay',
+  		},
+		{
+		path: '/programas/',
+    	url: 'programas.html',
+    	name: 'programas',
+  		},
+		{
+		path: '/deportes/',
+    	url: 'deportes.html',
+    	name: 'deportes',
+  		},
+		{
+		path: '/about/',
+    	url: 'about.html',
+    	name: 'about',
+  		},
+		{
+		path: '/setting/',
+    	url: 'setting.html',
+    	name: 'setting',
+  		},
+		{
+		path: '/aboutdulce/',
+    	url: 'aboutdulce.html',
+    	name: 'aboutdulce',
+  		},
+		{
+		path: '/aboutindira/',
+    	url: 'aboutindira.html',
+    	name: 'aboutindira',
+  		},
+		{
+		path: '/aboutdaniel/',
+    	url: 'aboutdaniel.html',
+    	name: 'aboutdaniel',
+  		},
+		{
+		path: '/aboutgeraldine/',
+    	url: 'aboutgeraldine.html',
+    	name: 'aboutgeraldine',
+  		},
+		{
+		path: '/tvcplay1/',
+    	url: 'tvcplay1.html',
+    	name: 'tvcplay1',
+  		},
+		{
+		path: '/5deportivo/',
+    	url: '5deportivo.html',
+    	name: '5deportivo',
+  		},
+		{
+		path: '/frente/',
+    	url: 'frente.html',
+    	name: 'frente',
+  		},
+		{
+		path: '/vivo/',
+    	url: 'vivo.html',
+    	name: 'vivo',
+  		},
+		{
+		path: '/novela1/',
+    	url: 'novela1.html',
+    	name: 'novela1',
+  		},
+		{
+		path: '/novela2/',
+    	url: 'novela2.html',
+    	name: 'novela2',
+  		},
+		{
+		path: '/novela3/',
+    	url: 'novela3.html',
+    	name: 'novela3',
+  		},
+		{
+		path: '/novela4/',
+    	url: 'novela4.html',
+    	name: 'novela4',
+  		},
+		{
+		path: '/5deportivo/',
+    	url: '5deportivo.html',
+    	name: '5deportivo',
+  		},
+		{
+		path: '/deporte1/',
+    	url: 'deporte1.html',
+    	name: 'deporte1',
+  		},
+		{
+		path: '/deporte2/',
+    	url: 'deporte2.html',
+    	name: 'deporte2',
+  		},
+		{
+		path: '/deporte3/',
+    	url: 'deporte3.html',
+    	name: 'deporte3',
+  		},
+		{
+		path: '/deporte4/',
+    	url: 'deporte4.html',
+    	name: 'deporte4',
+  		},
+		{
+		path: '/eltoque/',
+    	url: 'eltoque.html',
+    	name: 'eltoque',
+  		},
+		{
+		path: '/noticias1/',
+    	url: 'noticias1.html',
+    	name: 'noticias1',
+  		},
+		{
+		path: '/noticias2/',
+    	url: 'noticias2.html',
+    	name: 'noticias2',
+  		},
+		{
+		path: '/noticias3/',
+    	url: 'noticias3.html',
+    	name: 'noticias3',
+  		},
+		{
+		path: '/noticias4/',
+    	url: 'noticias4.html',
+    	name: 'noticias4',
+  		}
+	]
+});
+
+// Export selectors engine
+var $$ = Dom7;
+
+
+
+
+
+ 
+
